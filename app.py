56ea6b188efc733137962db7a2f590881011e1aa
Your Name <you@example.com>
2023-12-08 10:07:06 +0000

    first commit

---
+++ app.py
@@ -0,0 +1,90 @@
+import streamlit as st
+from dotenv import load_dotenv
+from PyPDF2 import PdfReader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings import CohereEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationalRetrievalChain
+from langchain.chat_models import ChatCohere
+from htmlTemplate import css, bot_template, user_template
+
+
+def get_pdf_text(pdf_docs):
+    text = ""
+    for pdf in pdf_docs:
+        pdf_reader = PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+    return text
+
+
+def split_text(text):
+    text_splitter = CharacterTextSplitter(
+        separator="\n",
+        chunk_size=1000,
+        chunk_overlap=200,
+        length_function=len
+    )
+    chunks = text_splitter.split_text(text)
+    return chunks
+
+
+def get_vectorstore(text_chunks):
+    embeddings = CohereEmbeddings(model="embed-english-v3.0")
+    vectorstore = FAISS.from_texts(text_chunks, embeddings)
+    return vectorstore
+
+
+def get_conversation_chain(vectorchain):
+    llm = ChatCohere()
+    memory = ConversationBufferMemory(memory_key='chat_history', return_messages=True)
+    conversation_chain = ConversationalRetrievalChain.from_llm(
+        llm = llm,
+        retriever=vectorchain.as_retriever(),
+        memory=memory
+    )
+    return conversation_chain
+    
+def handle_user_input(user_question):
+    response = st.session_state.conversation({'question': user_question})
+    st.session_state.chat_history = response['chat_history']
+
+    for i, message in enumerate(st.session_state.chat_history):
+        if i % 2 == 0:
+            st.write(user_template.replace(
+                "{{MSG}}", message.content), unsafe_allow_html=True)
+        else:
+            st.write(bot_template.replace(
+                "{{MSG}}", message.content), unsafe_allow_html=True)
+            
+
+def main():
+    load_dotenv()
+    st.set_page_config(page_title="Langchain PDF Reader")
+
+    st.write(css, unsafe_allow_html=True)
+
+    if "conversation" not in st.session_state:
+        st.session_state.conversation = None
+    if "chat_history" not in st.session_state:
+        st.session_state.chat_history = None
+
+    st.header("Chat")
+    user_question = st.text_input("Ask a question about your documents:")
+    if user_question:
+        handle_user_input(user_question)
+
+    with st.sidebar:
+        st.subheader("Uploaded Documents")
+        pdf_docs = st.file_uploader("Upload here:", accept_multiple_files=True)
+        if st.button("Upload"):
+            with st.spinner("Uploading"):
+                text = get_pdf_text(pdf_docs)
+                text_chunks = split_text(text)
+                vectorstore = get_vectorstore(text_chunks)
+                st.session_state.conversation = get_conversation_chain(vectorstore)
+
+
+if __name__ == '__main__':
+    main()
